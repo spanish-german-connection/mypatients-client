99fa980d64243123ef6bdcbf479e8ded16a53d16
julianBrandon <p0lterg3istt@gmail.com>
2022-10-28 22:27:29 +0200

    Add AuthContext + UseContext to the Navbar so that certain links show according to login status

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,23 +1,46 @@
 import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { AuthContext } from "../context/auth.context";
 
 function Navbar() {
+    const {
+        isLoggedIn,
+        user,
+        logOutUser
+    } = useContext(AuthContext);
+
     return (
         <nav>
             <Link to="/">
                 <button>Home</button>
             </Link>
-            <Link to="/login">
-                <button>Login</button>
-            </Link>
-            <Link to="/signup">
-                <button>SignUp</button>
-            </Link>
-            <Link to="/patients">
-                <button>Patients</button>
-            </Link>
-            <Link to="/appointments">
-                <button>Appointments</button>
-            </Link>
+
+            {isLoggedIn && (
+                <>
+                    <Link to="/patients">
+                        <button>Patients</button>
+                    </Link>
+                    <Link to="/appointments">
+                        <button>Appointments</button>
+                    </Link>
+                    <button onClick={logOutUser}>Logout</button>
+                    <span>{user && user.name}</span>
+                </>
+            )}
+
+            {!isLoggedIn && (
+                <>
+
+                    <Link to="/signup">
+                        <button>SignUp</button>
+                    </Link>
+                    <Link to="/login">
+                        <button>Login</button>
+                    </Link>
+
+                </>
+            )}
+
         </nav>
 
     )
