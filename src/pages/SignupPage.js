5c470c7c89f5d6cd19f5e9cd515649ec14e2691a
Gabriel Gomez <ggomez.box@gmail.com>
2022-10-31 14:00:09 +0100

    add signupPage with validations

--- src/pages/SignupPage.js
+++ src/pages/SignupPage.js
@@ -1,36 +1,27 @@
 import { Alert, Button, Checkbox, Divider, Form, Input } from "antd";
 import axios from "axios";
-import { useContext, useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
-import { AuthContext } from "../context/auth.context";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-function LoginPage(props) {
+function SignupPage() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
   const [errorMessage, setErrorMessage] = useState(undefined);
 
   const navigate = useNavigate();
-  const { storeToken, authenticateUser } = useContext(AuthContext);
 
   const handleEmail = (e) => setEmail(e.target.value);
   const handlePassword = (e) => setPassword(e.target.value);
+  const handleName = (e) => setName(e.target.value);
 
-  const handleLoginSubmit = () => {
-    const requestBody = { email, password };
+  const handleSignupSubmit = () => {
+    const requestBody = { email, password, name };
 
     axios
-      .post(`${process.env.REACT_APP_API_URL}/auth/login`, requestBody)
-      .then((response) => {
-        // Request to the server's endpoint `/auth/login` returns a response
-        // with the JWT string ->  response.data.authToken
-        console.log("JWT token", response.data.authToken);
-        storeToken(response.data.authToken);
-
-        // Verify the token by sending a request
-        // to the server's JWT validation endpoint.
-        authenticateUser();
-
-        navigate("/");
+      .post(`${process.env.REACT_APP_API_URL}/auth/signup`, requestBody)
+      .then(() => {
+        navigate("/login");
       })
       .catch((error) => {
         const errorDescription = error.response.data.message;
@@ -54,10 +45,10 @@ function LoginPage(props) {
         labelCol={{ span: 10 }}
         wrapperCol={{ span: 4 }}
         initialValues={{ remember: true }}
-        onFinish={handleLoginSubmit}
+        onFinish={handleSignupSubmit}
         autoComplete="off"
       >
-        <Divider>Login</Divider>
+        <Divider>Signup</Divider>
         <Form.Item
           label="Email:"
           type="email"
@@ -65,7 +56,7 @@ function LoginPage(props) {
           value={email}
           onChange={handleEmail}
           rules={[
-            { required: true, message: "Please input your username!" },
+            { required: true, message: "Please input your email!" },
             {
               type: "email",
               warningOnly: false,
@@ -95,6 +86,24 @@ function LoginPage(props) {
         </Form.Item>
 
         <Form.Item
+          label="Name:"
+          type="name"
+          name="name"
+          value={name}
+          onChange={handleName}
+          rules={[
+            { required: true, message: "Please input your name!" },
+            {
+              type: "string",
+              min: 3,
+              warningOnly: true,
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item
           name="remember"
           valuePropName="checked"
           wrapperCol={{ offset: 9, span: 6 }}
@@ -108,11 +117,8 @@ function LoginPage(props) {
           </Button>
         </Form.Item>
       </Form>
-
-      <p>Don't have an account yet?</p>
-      <Link to={"/signup"}> Sign Up</Link>
     </div>
   );
 }
 
-export default LoginPage;
+export default SignupPage;
