51d17946e28bbeb09d1b2d49322a9a410bdc5dfc
Gabriel Gomez <ggomez.box@gmail.com>
2022-11-02 17:07:14 +0100

    show api errors on alert for appointments actions

--- src/components/AppointmentCard.js
+++ src/components/AppointmentCard.js
@@ -1,12 +1,15 @@
 import { ExclamationCircleOutlined } from "@ant-design/icons";
 import {
+  Alert,
   Button,
+  Col,
   DatePicker,
   Divider,
   Form,
   Input,
   Modal,
   Radio,
+  Row,
   Select,
   Switch,
 } from "antd";
@@ -23,6 +26,8 @@ function AppointmentCard({
   refreshAppointments,
 }) {
   const [patients, setPatients] = useState(null);
+  const [errorMessage, setErrorMessage] = useState(null);
+
   const [form] = Form.useForm();
 
   useEffect(() => {
@@ -55,9 +60,11 @@ function AppointmentCard({
     form.resetFields();
     setShowForm(false);
     refreshAppointments();
+    setErrorMessage(null);
   };
 
   const handleSubmit = (inputs) => {
+    console.log("inputs>>>", inputs);
     if (appointmentToEdit) {
       axios
         .put(
@@ -66,7 +73,11 @@ function AppointmentCard({
           getAuthHeader()
         )
         .then(() => initializeForm())
-        .catch((error) => console.log(error));
+        .catch((error) => {
+          console.log(error);
+          const errorDescription = error.response.data.message;
+          setErrorMessage(errorDescription);
+        });
     } else {
       axios
         .post(
@@ -75,7 +86,11 @@ function AppointmentCard({
           getAuthHeader()
         )
         .then(() => initializeForm())
-        .catch((error) => console.log(error));
+        .catch((error) => {
+          console.log(error);
+          const errorDescription = error.response.data.message;
+          setErrorMessage(errorDescription);
+        });
     }
   };
 
@@ -86,7 +101,11 @@ function AppointmentCard({
         getAuthHeader()
       )
       .then(() => initializeForm())
-      .catch((error) => console.log(error));
+      .catch((error) => {
+        console.log(error);
+        const errorDescription = error.response.data.message;
+        setErrorMessage(errorDescription);
+      });
   };
 
   const showDeleteConfirm = () => {
@@ -106,98 +125,125 @@ function AppointmentCard({
     });
   };
 
+  const disabledDate = (current) => {
+    // Can not select days before today and today
+    return current < moment().subtract(1, "days");
+  };
+
+  const disabledDateTime = () => ({
+    disabledHours: () => [...Array(8).keys(), 22, 23].filter((hour) => hour),
+  });
+
   return (
-    <Form
-      form={form}
-      labelCol={{ span: 8 }}
-      wrapperCol={{ span: 8 }}
-      onFinish={handleSubmit}
-      autoComplete="off"
-      initialValues={{
-        patientId: "",
-        date: "",
-        isPaid: false,
-        recurring: "",
-        notes: "",
-      }}
-    >
-      <Divider>Add Appointment</Divider>
-      <Form.Item hidden name="id">
-        <Input type="hidden" />
-      </Form.Item>
-      <Form.Item
-        label="Patient:"
-        name="patientId"
-        rules={[{ required: true, message: "Please select a patient!" }]}
-        className="align-left"
+    <>
+      {errorMessage && (
+        <Row>
+          <Col span="8" offset="8">
+            <Alert
+              message="There was an error"
+              description={errorMessage}
+              type="error"
+              showIcon
+            />
+          </Col>
+        </Row>
+      )}
+      <Form
+        form={form}
+        labelCol={{ span: 8 }}
+        wrapperCol={{ span: 8 }}
+        onFinish={handleSubmit}
+        autoComplete="off"
+        initialValues={{
+          patientId: "",
+          date: "",
+          isPaid: false,
+          recurring: "",
+          notes: "",
+        }}
       >
-        <Select
-          placeholder="Select a patient"
-          optionFilterProp="children"
-          showSearch
-          // value={"test"}
-          filterOption={(input, option) =>
-            option?.children.toLowerCase().includes(input.toLowerCase())
-          }
-          disabled={appointmentToEdit}
+        <Divider>Add Appointment</Divider>
+        <Form.Item hidden name="id">
+          <Input type="hidden" />
+        </Form.Item>
+        <Form.Item
+          label="Patient:"
+          name="patientId"
+          rules={[{ required: true, message: "Please select a patient!" }]}
+          className="align-left"
         >
-          {patients &&
-            patients.map((patient) => (
-              <Select.Option key={patient._id} value={patient._id} required>
-                {`${patient.name} ${patient.surname}`}
-              </Select.Option>
-            ))}
-        </Select>
-      </Form.Item>
-      <Form.Item
-        label="Date:"
-        className="align-left"
-        name="date"
-        rules={[{ required: true, message: "Please select a date!" }]}
-      >
-        <DatePicker
-          showTime={{
-            defaultValue: moment("09:00", "HH:mm"),
-            format: "HH:mm",
-          }}
-        />
-      </Form.Item>
-      <Form.Item
-        label="is Paid?"
-        name="isPaid"
-        className="align-left"
-        valuePropName="checked"
-      >
-        <Switch />
-      </Form.Item>
-      <Form.Item
-        label="Recurring?"
-        className="align-left"
-        name="recurring"
-        rules={[
-          { required: true, message: "Please select if it is recurring!" },
-        ]}
-      >
-        <Radio.Group>
-          <Radio value="none">none</Radio>
-          <Radio value="weekly">weekly</Radio>
-          <Radio value="biweekly">biweekly</Radio>
-        </Radio.Group>
-      </Form.Item>
-      <Form.Item label="Notes:" name="notes">
-        <TextEditor />
-      </Form.Item>
-      <Form.Item wrapperCol={{ offset: 9, span: 6 }}>
-        <Button type="primary" htmlType="submit">
-          {appointmentToEdit ? "Update" : "Create"}
-        </Button>
-        {appointmentToEdit && (
-          <Button onClick={showDeleteConfirm} danger>
-            Delete
+          <Select
+            placeholder="Select a patient"
+            optionFilterProp="children"
+            showSearch
+            // value={"test"}
+            filterOption={(input, option) =>
+              option?.children.toLowerCase().includes(input.toLowerCase())
+            }
+            disabled={appointmentToEdit}
+          >
+            {patients &&
+              patients.map((patient) => (
+                <Select.Option key={patient._id} value={patient._id} required>
+                  {`${patient.name} ${patient.surname}`}
+                </Select.Option>
+              ))}
+          </Select>
+        </Form.Item>
+        <Form.Item
+          label="Date:"
+          className="align-left"
+          name="date"
+          rules={[{ required: true, message: "Please select a date!" }]}
+          allowClear="false"
+        >
+          <DatePicker
+            showTime={{
+              defaultValue: moment("09:00", "HH:mm"),
+              format: "HH:mm",
+            }}
+            disabledDate={disabledDate}
+            disabledTime={disabledDateTime}
+          />
+        </Form.Item>
+        <Form.Item
+          label="is Paid?"
+          name="isPaid"
+          className="align-left"
+          valuePropName="checked"
+        >
+          <Switch />
+        </Form.Item>
+        <Form.Item
+          label="Recurring?"
+          className="align-left"
+          name="recurring"
+          rules={[
+            { required: true, message: "Please select if it is recurring!" },
+          ]}
+        >
+          <Radio.Group>
+            <Radio value="none">none</Radio>
+            <Radio value="weekly">weekly</Radio>
+            <Radio value="biweekly">biweekly</Radio>
+          </Radio.Group>
+        </Form.Item>
+        <Form.Item label="Notes:" name="notes">
+          <TextEditor />
+        </Form.Item>
+
+        <Form.Item wrapperCol={{ offset: 9, span: 6 }}>
+          <Button type="primary" htmlType="submit">
+            {appointmentToEdit ? "Update" : "Create"}
           </Button>
-        )}
-      </Form.Item>
-    </Form>
+          {appointmentToEdit && (
+            <Button onClick={showDeleteConfirm} danger>
+              Delete
+            </Button>
+          )}
+        </Form.Item>
+      </Form>
+    </>
   );
 }
 
