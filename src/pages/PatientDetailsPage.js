e32c017d5e651b45dfb9bed9d60c2f7dbc7077d5
Gabriel Gomez <ggomez.box@gmail.com>
2022-11-03 19:31:42 +0100

    set patient detail page look&feel more similar to forms

--- src/pages/PatientDetailsPage.js
+++ src/pages/PatientDetailsPage.js
@@ -1,109 +1,137 @@
-import axios from 'axios';
+import axios from "axios";
 import moment from "moment/moment";
-import { useEffect, useState } from 'react';
-import { Link, useParams } from 'react-router-dom';
+import { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
 import getAuthHeader from "../utils/token";
 import "./PatientDetailsPage.css";
-import {
-    Button,
-    Col,
-    Divider,
-    Form,
-    Row,
-    Typography,
-} from "antd";
+import { Button, Col, Divider, Form, Row, Typography } from "antd";
 
 const API_URL = process.env.REACT_APP_API_URL;
 
 function PatientDetailsPage() {
-    const [patient, setPatient] = useState(null);
-    const { patientId } = useParams();
+  const [patient, setPatient] = useState(null);
+  const { patientId } = useParams();
 
-    useEffect(() => {
-        fetchPatient();
-    }, []);
+  useEffect(() => {
+    fetchPatient();
+  }, []);
 
-    const fetchPatient = () => {
-        axios
-            .get(
-                `${API_URL}/api/patients/${patientId}`,
-                getAuthHeader()
-            )
-            .then(response => {
-                const onePatient = response.data;
-                setPatient(onePatient);
-            })
-            .catch(error => console.log(error));
-    };
+  const fetchPatient = () => {
+    axios
+      .get(`${API_URL}/api/patients/${patientId}`, getAuthHeader())
+      .then((response) => {
+        const onePatient = response.data;
+        setPatient(onePatient);
+      })
+      .catch((error) => console.log(error));
+  };
 
-    return (
-        <div className="PatientDetails">
-            {patient && (
-                <>
-                    <Divider>Patient details</Divider>
-                    <Row className="row-patient-detail">
-                        {/* <Col> */}
-                            <Col span="8" offset="0">
-                            <Typography className="pd-flex">
-                                <span className="pd-label">Name:</span>
-                                <pre className="align-left">{patient.name}</pre>
-                            </Typography>
-                            <Typography className="pd-flex">
-                                <span className="pd-label">Surname:</span>
-                                <pre className="align-left">{patient.surname}</pre>
-                            </Typography>
-                            <Typography className="pd-flex">
-                                <span className="pd-label">Date of birth:</span>
-                                <pre className="align-left">{moment(patient.dateOfBirth).format(
-                                    "DD-MMM-YYYY"
-                                )}</pre>
-                            </Typography>
-                            <Typography className="pd-flex">
-                                <span className="pd-label">Therapist:</span>
-                                <pre className="align-left">{patient.therapist.name}</pre>
-                            </Typography>
+  return (
+    <div className="PatientDetails">
+      {patient && (
+        <>
+          <Divider>Patient details</Divider>
+          <Row className="row-patient-detail">
+            <Col span="8" offset="0" className="pd-column-label">
+              <span className="pd-label">Name:</span>
+            </Col>
+            <Col span="8">
+              <Typography className="pd-flex">
+                <pre className="align-left">{patient.name}</pre>
+              </Typography>
+            </Col>
+          </Row>
+          <Row className="row-patient-detail">
+            <Col span="8" offset="0" className="pd-column-label">
+              <span className="pd-label">Surname:</span>
+            </Col>
+            <Col span="8">
+              <Typography className="pd-flex">
+                <pre className="align-left">{patient.surname}</pre>
+              </Typography>
+            </Col>
+          </Row>
+          <Row className="row-patient-detail">
+            <Col span="8" offset="0" className="pd-column-label">
+              <span className="pd-label">Date of birth:</span>
+            </Col>
+            <Col span="8">
+              <Typography className="pd-flex">
+                <pre className="align-left">
+                  {moment(patient.dateOfBirth).format("DD-MMM-YYYY")}
+                </pre>
+              </Typography>
+            </Col>
+          </Row>
+          <Row className="row-patient-detail">
+            <Col span="8" offset="0" className="pd-column-label">
+              <span className="pd-label">Therapist:</span>
+            </Col>
+            <Col span="8">
+              <Typography className="pd-flex">
+                <pre className="align-left">{patient.therapist.name}</pre>
+              </Typography>
+            </Col>
+          </Row>
+          <Row className="row-patient-detail">
+            <Col span="8" offset="0" className="pd-column-label">
+              <span className="pd-label">Email-Address:</span>
+            </Col>
+            <Col span="8">
+              <Typography className="pd-flex">
+                <pre className="align-left">{patient.email}</pre>
+              </Typography>
+            </Col>
+          </Row>
+          <Row className="row-patient-detail">
+            <Col span="8" offset="0" className="pd-column-label">
+              <span className="pd-label">Diagnoses:</span>
+            </Col>
+            <Col span="8">
+              <Typography className="pd-flex">
+                <pre className="align-left">
+                  <span
+                    className="pd-large-text"
+                    dangerouslySetInnerHTML={{
+                      __html: patient.diagnoses ? patient.diagnoses : "None",
+                    }}
+                  ></span>
+                </pre>
+              </Typography>
+            </Col>
+          </Row>
+          <Row className="row-patient-detail">
+            <Col span="8" offset="0" className="pd-column-label">
+              <span className="pd-label">Medications:</span>
+            </Col>
+            <Col span="8">
+              <Typography className="pd-flex">
+                <pre className="align-left pd-large-text">
+                  <span
+                    dangerouslySetInnerHTML={{
+                      __html: patient.medications
+                        ? patient.medications
+                        : "None",
+                    }}
+                  ></span>
+                </pre>
+              </Typography>
+            </Col>
+          </Row>
 
-                            <Typography className="pd-flex">
-                                <span className="pd-label">Email-Address:</span>
-                                <pre className="align-left">{patient.email}</pre>
-                            </Typography>
+          <Link to={`/patient/edit/${patientId}`}>
+            <Button className="btn-margin" type="primary">
+              Edit Patient
+            </Button>
+          </Link>
 
-
-                            <Typography className="pd-flex">
-                                <span className="pd-label">Diagnoses:</span>
-                                <pre className="align-left">
-                                    <span dangerouslySetInnerHTML={{ __html: patient.diagnoses }}>
-                                    </span>
-                                </pre>
-                            </Typography>
-                            <Typography className="pd-flex">
-                                <span className="pd-label">Medications:</span>
-                                <pre className="align-left">
-                                    <span dangerouslySetInnerHTML={{ __html: patient.medications }}>
-                                    </span>
-                                </pre>
-
-
-
-                            </Typography>
-
-                        </Col>
-                    </Row>
-
-                    <Link to={`/patient/edit/${patientId}`} >
-                        <Button className='btn-margin' type="primary">Edit Patient</Button>
-                    </Link>
-
-                    <Link to="/patients">
-                        <Button className='btn-margin'>Back</Button>
-                    </Link>
-
-
-
-                </>
-            )}
-        </div>
-    )
+          <Link to="/patients">
+            <Button className="btn-margin">Back</Button>
+          </Link>
+        </>
+      )}
+    </div>
+  );
 }
 
 export default PatientDetailsPage;
